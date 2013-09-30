1c32d3f7661ea8bb723fa9af3a19668b378cfa1d
Ivan Storck <ivanoats@gmail.com>
2013-09-29 22:19:47 -0700

    rush-ing a blog

---
+++ db/migrations/20130929221410_create_posts.js
@@ -0,0 +1,31 @@
+var CreatePosts = function () {
+  this.up = function (next) {
+    var def = function (t) {
+          t.column('title', 'string');
+          t.column('body', 'text');
+        }
+      , callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.createTable('post', def, callback);
+  };
+
+  this.down = function (next) {
+    var callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.dropTable('post', callback);
+  };
+};
+
+exports.CreatePosts = CreatePosts;
